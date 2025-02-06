f85798c87af24f5a93ff1f2fbc9c5ab1f4c539cc
Sambhav-Varshney <svvarshney649@gmail.com>
2025-02-06 09:54:45 +0530

    Add files via upload

---
+++ stringComparision.java
@@ -0,0 +1,7 @@
+class stringComparison {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "hello";
+        System.out.println("Are strings equal (ignoring case)? " + s1.equalsIgnoreCase(s2));
+    }
+}
